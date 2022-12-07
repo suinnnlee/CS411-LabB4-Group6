1e9f6158fa2cd5edd2606056e3596239f446fece
Jessica Su <103076065+Jessicasu1023@users.noreply.github.com>
2022-12-06 21:17:49 -0500

    Update ticketmaster.py

--- flaskProject/ticketmaster.py
+++ flaskProject/ticketmaster.py
@@ -1,51 +1,117 @@
-from urllib.request import urlopen
-from flask import Flask, jsonify, request
-import requests
+import xdrlib
+from xml.dom import xmlbuilder
+from xml.sax import default_parser_list
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, url_for, session, request, redirect
 import json
+import time
+import pandas as pd
+import re
 
+from ticketmaster import get_events
+
+# App config
 app = Flask(__name__)
 
-apikey = 's2oO5t5X9U4lnJ5BMtzAGSAGWBSlU9zk' #hide on repo
+app.secret_key = '837e4b8f92eb45b787daf6c243dfae8c'
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
+TOKEN_INFO ="token_info"
 
 
+@app.route('/')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    print(auth_url)
+    return redirect(auth_url)
 
-# @app.route("/get_events", methods=['GET', 'POST'])
-# def events():
-#     if request.method == 'POST':
-#         # store the file that is passed in with the POST request
-#         file = request.files['file']
-#         data = json.load(file)
-#     else:  # For debugging, if script.py didnt make a POST request, just manually load the file
-#         with open('request.json') as f:
-#             data = json.load(f)
 
-#     # Payload that will be sent to the ticketmaster API
-#     payload = {"size": data['size'], "city": data['city'], "apikey": apikey}
-#     # Make a request to the ticketmaster api given the URL and the parameters
-#     search = requests.get(search_url, params=payload)
+@app.route('/authorize')
+def authorize():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    return redirect("/getTracks")
 
-#     # Turn response into json
-#     search_json = search.json()
 
-#     search_json_filtered = {
-#         "name": search_json['_embedded']['events'][0]['name'],
-#         "url": search_json['_embedded']['events'][0]['url']
-#     }
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
 
-#     return jsonify(json.dumps(search_json_filtered, indent = 4)) 
 
-def get_events(artist):
-    print(type(artist))
-    search_url= 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&keyword='+artist+'&apikey=s2oO5t5X9U4lnJ5BMtzAGSAGWBSlU9zk'
-    response = urlopen(search_url)
-    data_json = json.loads(response.read())
+@app.route('/getTracks')
+def get_all_tracks():
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    results = []
+    iter = 0
+    while True:
+        offset = iter * 50
+        iter += 1
+        curGroup = sp.current_user_saved_tracks(limit=50, offset=offset)['items']
+        for idx, item in enumerate(curGroup):
+            track = item['track']
+            val = track['artists'][0]['name']
+            results += [val]
+        if (len(curGroup) < 50):
+            break
+    # '''df = pd.DataFrame(results, columns=["song names"])
+    # df.to_csv('songs.csv', index=False)'''
+    # '''results=re.sub(r"\s+", '-', results)'''
+    string=""
+    counter =0 
+    for artist in results:
+        artist_name=string.join(artist)
+        result = artist_name.replace(' ', '_')
+        get_events(result)
+    return search_url
+    # return get_events(results[1])
 
-    return data_json
+def get_concerts():
+    results = get_all_tracks()
+    for i in results:
+        print(i)
+    return results
+
+# Checks to see if token is valid and gets a new token if not
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
+
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id="1933a86907714b4a97765c8836a4fd00",
+        client_secret="837e4b8f92eb45b787daf6c243dfae8c",
+        redirect_uri=url_for('authorize', _external=True),
+        scope="user-library-read")
 
 
 if __name__ == '__main__':
-    app.run(debug=True) #set to false on production
+    app.run()
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
