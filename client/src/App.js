a52feeb63fca76cad8eb5e7f8fd0303acd66303c
lukasmwerk <lukasmwerk@gmail.com>
2022-12-09 11:13:32 -0500

    message

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,23 @@
 // import BookData from "./Data.json";
 //import { useEffect, useState } from 'react';
 
+import axios from 'axios'
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages
 import Home from './pages/home/Home'
 import Dashboard from './pages/dashboard/Dashboard'
+
+const checkLoginStatus = () => {
+  axios
+    .get("http://localhost:3000/loggedIn", {withCredentials: true})
+    .then(response => {
+      console.log("logged in?", response);
+    })
+    .catch(error => {
+      console.log("check login error", error);
+    })
+}
 
 function App() {
   return (
