fcb13efdd055906e8e0c3ba624e435bf098a5905
suinnnlee <91098563+suinnnlee@users.noreply.github.com>
2022-12-09 13:56:06 -0500

    Delete App.py

--- App.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://mchoe:ForSchool23%21@cs411b4g6.e9hxzy4.mongodb.net/test')
-# Creating a database name GFG
-db = client['mydb']
-# Col = db[‘concerts']
